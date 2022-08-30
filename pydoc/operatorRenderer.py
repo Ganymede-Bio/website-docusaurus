05624c364284e5179d0e5316ea04dd03437f7a5e
Andy Caruso <andy@ganymede.bio>
2022-08-29 20:07:49 -0700

    Add some explanation for doc overrides

--- pydoc/operatorRenderer.py
+++ pydoc/operatorRenderer.py
@@ -1,4 +1,8 @@
 # operatorRenderer.py
+# We need to override the defaults for pydoc and docusaurus since we don't want
+# to display file names to users, only class names.
+# This is based off of this example:
+# https://github.com/NiklasRosenstein/pydoc-markdown/discussions/203#discussioncomment-1192275
 
 import dataclasses
 import docspec
@@ -14,12 +18,15 @@ class OperatorRenderer(DocusaurusRenderer):
 
   def init(self, context: Context) -> None:
     self.markdown.init(context)
+    # Save the old header renderer and wrap it in our customization
     self.render_header = self.markdown._render_header
     self.markdown._render_header = self.custom_header
 
 
   def custom_header(self, fp: t.TextIO, level: int, obj: docspec.ApiObject):
     if isinstance(obj, docspec.Class):
+        # If this is a class definition, use it as the header 
+        # see https://github.com/NiklasRosenstein/pydoc-markdown/blob/b15127e3c643976e71a10c7fa4d03297ee616542/src/pydoc_markdown/contrib/renderers/docusaurus.py#L31
         code = ("---\n"
         f"sidebar_label: {obj.name}\n"
         f"title: {obj.name}\n"
