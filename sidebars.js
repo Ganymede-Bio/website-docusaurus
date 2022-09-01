2fa49726643a5cb686982db3e647defd0142615b
daniellekuhn <danielle.r.kuhn@gmail.com>
2022-08-31 20:10:55 -0500

    Add section for Files

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Getting Started',
-      items: ['Introduction', 'FlowEditor', 'FlowRuns'],
+      items: ['Introduction', 'FlowEditor', 'FlowRuns', 'Files'],
       // Features: ['mdx'],
     },
   ],
