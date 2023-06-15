a17b1744082381da2e4c77d12cb17e9f2b20d386
Benson Lee <bensonlee5@gmail.com>
2023-06-15 11:10:01 -0700

    upd

--- sidebars.js
+++ sidebars.js
@@ -20,7 +20,7 @@ module.exports = {
       },
       {
         type: 'category',
-        label: 'Quickstarts',
+        label: 'Quickstart Guides',
         collapsed: false,
         items: [
           {
