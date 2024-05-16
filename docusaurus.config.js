f7d412f4990d0b2dc2788b995809e8da00fc60bb
Benson Lee <bensonlee5@gmail.com>
2024-05-16 11:46:12 -0700

    remove releases from search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
       apiKey: 'd7fb51ca85e14d48eb21e1fd4e08c1f6',
       indexName: 'ganymede',
       contextualSearch: false,
-      exclusionPatterns: ['**/releases/*'],
+      exclusionPatterns: ['https://docs.ganymede.bio/releases/**'],
     },
     navbar: {
       title: 'Ganymede',
