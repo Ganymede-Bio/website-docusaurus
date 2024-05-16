d0499753c6dc9dab2b14feae04d633399acf8bf1
Benson Lee <bensonlee5@gmail.com>
2024-05-16 11:56:50 -0700

    bury rather than remove releaes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
       apiKey: 'd7fb51ca85e14d48eb21e1fd4e08c1f6',
       indexName: 'ganymede',
       contextualSearch: false,
-      exclusionPatterns: ['https://docs.ganymede.bio/releases/**'],
+      optionalFilters: ['category:-releases'],
     },
     navbar: {
       title: 'Ganymede',
