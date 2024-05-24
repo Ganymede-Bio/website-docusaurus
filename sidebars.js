49562483bb4e90eba562d4ed27549b8abd6833dd
Benson Lee <bensonlee5@gmail.com>
2024-05-24 10:53:37 -0700

    add note on hovering cursor over session for virtualization

--- sidebars.js
+++ sidebars.js
@@ -78,28 +78,34 @@ module.exports = {
         label: 'Processing Data',
         collapsed: true,
         items: [{
-          type: 'category',
-          label: 'Running Flows',
-          collapsed: false,
-          items: ['Home', 'FlowView']
-        },
-        {
-          type: 'category',
-          label: 'Editing Flows',
-          collapsed: false,
-          items: ['FlowEditor', 'NodeEditor', 'SystemMetadata', 'DebuggingFlows'],
-        },
-        {
-          type: 'category',
-          label: 'Monitoring Flows',
-          collapsed: true,
-          items: ['FlowRuns', 'Notifications']
-        },
-        {
+            type: 'category',
+            label: 'Running Flows',
+            collapsed: false,
+            items: ['Home', 'FlowView']
+          },
+          {
+            type: 'category',
+            label: 'Editing Flows',
+            collapsed: false,
+            items: ['FlowEditor', 'NodeEditor', 'SystemMetadata'],
+          },
+          {
+            type: 'category',
+            label: 'Monitoring Flows',
+            collapsed: true,
+            items: ['FlowRuns', 'Notifications']
+          },
+          {
           type: 'doc',
           id: 'Virtualization',
           label: 'Virtualization'
-        }]
+          },
+          {
+            type: 'doc',
+            id: 'TroubleshootingDataProcessing',
+            label: 'Troubleshooting'
+          }
+        ]
       },
       {
         type: 'category',
